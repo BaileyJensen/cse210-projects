7adca82d785447d653a9944f4160ad4f8c2263e1
BaileyJensen <bjensen4@byupathway.edu>
2026-05-08 16:59:46 -0600

    W01 Exercise 04 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+using System.Linq;
+ 
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+    int userInput = -1;
+        List<int> numbers = new List<int>();
+    do
+    {
+        Console.WriteLine("Please enter a list of numbers. Type '0' when done.");
+        userInput = int.Parse(Console.ReadLine());
+        if (userInput != 0)
+        {
+        numbers.Add(userInput);
+        }
+        int count =(numbers.Count);
+    } while (userInput != 0); 
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+        double average = ((double)sum/numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
     }
+
 }
