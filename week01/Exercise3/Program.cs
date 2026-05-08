6222cd388c51f56c7d407569648b6a74bc4ad607
BaileyJensen <bjensen4@byupathway.edu>
2026-05-08 11:36:26 -0600

    W01 Exercise 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magNum = randomGenerator.Next(1, 101);
+
+
+    int number = 0;
+    do
+    {
+   
+        Console.WriteLine("What is your guess?");
+        string guess = Console.ReadLine();
+        number = int.Parse(guess);
+        Console.WriteLine($"Your guess is {guess}");
+
+
+        if (number > magNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (number < magNum)
+        {
+            Console.WriteLine("Higher");
+        }
+        else 
+        {
+            Console.WriteLine("You guessed it!");
+        }
+    } while (number != magNum);
     }
 }
