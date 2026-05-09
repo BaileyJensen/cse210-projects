611587044f45c3a9116ac9674454000513c21458
BaileyJensen <bjensen4@byupathway.edu>
2026-05-08 20:27:24 -0600

    W01 Exercise 05

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int numberSquared =SquareNumber(number);
+        DisplayResults(name , numberSquared);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        string favNum = Console.ReadLine();
+        int number = int.Parse(favNum);
+        return number;
+
+    }
+    static int SquareNumber(int number)
+    {
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+
+    static void DisplayResults(string name, int numberSquared )
+    {
+        Console.WriteLine($"{name}, the square of your number is {numberSquared}");
+        
     }
 }
